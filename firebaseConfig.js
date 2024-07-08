79e3b53ed8aed79f62a1072704b38564e4957023
deshan68 <arundeshan@gmail.com>
2024-07-08 18:49:36 +0530

    [Minor] Update DB

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId: "my-portfolio-db-4f84e",
+  projectId: "learnfb-bc663",
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
