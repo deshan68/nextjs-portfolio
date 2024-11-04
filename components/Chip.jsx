b28b220916c67a3b72c284bdb58518af37131d66
deshan68 <arundeshan@gmail.com>
2024-11-04 23:14:17 +0530

    [minor]

--- components/Chip.jsx
+++ components/Chip.jsx
@@ -4,7 +4,7 @@ const Chip = ({ item }) => {
   return (
     <div
       key={item}
-      className="rounded-full bg-teal-300/10 text-teal-300 px-3 leading-3 text-base pt-2 pb-[4.5px]"
+      className="rounded-full bg-teal-300/10 text-teal-300 px-3 leading-3 text-sm pt-2 pb-[4.5px]"
     >
       {item}
     </div>
--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -1,8 +1,10 @@
+import { wso2_experience } from "@constants";
+
 const { default: Chip } = require("./Chip");
 
 const ExperienceCard = () => {
   return (
-    <div className="text-base px-5 cursor-pointer flex w-full gap-x-5 transition ease-in-out duration-500 transform hover:bg-white/5 hover:backdrop-blur-lg hover:shadow-lg rounded-lg p-5">
+    <div className="text-base px-5 cursor-default flex w-full gap-x-5 transition ease-in-out duration-500 transform hover:bg-white/5 hover:backdrop-blur-lg hover:shadow-lg rounded-lg p-5">
       <div className="w-1/5">JAN - JUN 2024</div>
       <div className="w-4/5 flex flex-col gap-y-3">
         <div className="text-white font-bold">
@@ -14,19 +16,23 @@ const ExperienceCard = () => {
             </a>
           </span>
         </div>
-        <p className="leading-tight">
-          Developed an Application Tracking System: This included creating job
-          requisitions, assigning recruiters, managing vacancies, publishing
-          them on the career webpage, adding candidates, reviewing CVs. During
-          the development process, I handled both the front-end as well as the
-          back- end, ensuring that both functions worked seamlessly together.
-        </p>
+        <ul class="list-disc">
+          {wso2_experience.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
         <div className="flex text-xs pt-3 flex-wrap gap-2">
-          {["TypeScript", "Ballerina", "React.js", "MUI", "MySQL"].map(
-            (item) => (
-              <Chip key={item} item={item} />
-            )
-          )}
+          {[
+            "TypeScript",
+            "Ballerina",
+            "React",
+            "Redux",
+            "MUI",
+            "MySQL",
+            "Choreo",
+          ].map((item) => (
+            <Chip key={item} item={item} />
+          ))}
         </div>
       </div>
     </div>
